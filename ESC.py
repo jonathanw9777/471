8e2b38d16aa7f91325a722c69059eb66f5e2d5d8
Cesar Martinez Jr <54192054+CesMartzJr@users.noreply.github.com>
2021-02-05 16:41:55 -0800

    Update ESC.py

--- ESC.py
+++ ESC.py
@@ -1,24 +1,3 @@
-#import RPi.GPIO as GPIO
-#from time import sleep
-
-#GPIO.setmode(GPIO.BCM)
-
-#GPIO.setup(18,GPIO.OUT)
-#motor=GPIO.PWM(18,100)
-#motor.start(0)
-
-# GPIO.setwarnings(False)
-
-#try:
-#    for i in range(100):
-#        print(i)
-#        motor.ChangeDutyCycle(i)
-#        sleep(0.5)
-#except KeyboardInterrupt:
-#    print("Motor kapatılıyor... (Ctrl+C ile çıkış yapıldı.)")
-#finally:
-#    GPIO.cleanup()
-
 import RPi.GPIO as GPIO
 import time
  
@@ -28,7 +7,7 @@ def setup():
     global pwm
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(ledPin, GPIO.OUT)
-    GPIO.output(ledPin, GPIO.LOW)
+    GPIO.output(ledPin, GPIO.HIGH)
     pwm = GPIO.PWM(ledPin, 1700) # Set Frequency to 1 KHz
     pwm.start(50) # Set the starting Duty Cycle
     print("Starting Duty Cycle at 50")
