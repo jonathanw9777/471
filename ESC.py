c1858ccaf9c91dbb30fcc072a748caf105546d5d
Cesar Martinez Jr <54192054+CesMartzJr@users.noreply.github.com>
2021-02-05 16:11:25 -0800

    Update ESC.py

--- ESC.py
+++ ESC.py
@@ -29,7 +29,7 @@ def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(ledPin, GPIO.OUT)
     GPIO.output(ledPin, GPIO.LOW)
-    pwm = GPIO.PWM(ledPin, 1000) # Set Frequency to 1 KHz
+    pwm = GPIO.PWM(ledPin, 1900) # Set Frequency to 1 KHz
     pwm.start(0) # Set the starting Duty Cycle
      
 def loop():
