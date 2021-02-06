5535447ecd6efb0a7d4a0ecec10683730481e6b0
Cesar Martinez Jr <54192054+CesMartzJr@users.noreply.github.com>
2021-02-05 16:15:17 -0800

    Update ESC.py

--- ESC.py
+++ ESC.py
@@ -30,18 +30,18 @@ def setup():
     GPIO.setup(ledPin, GPIO.OUT)
     GPIO.output(ledPin, GPIO.LOW)
     pwm = GPIO.PWM(ledPin, 1900) # Set Frequency to 1 KHz
-    pwm.start(0) # Set the starting Duty Cycle
+    pwm.start(50) # Set the starting Duty Cycle
      
 def loop():
     while True:
-        for dc in range(0, 101, 1):
-            pwm.ChangeDutyCycle(dc)
-            time.sleep(0.01)
-        time.sleep(1)
-        for dc in range(100, -1, -1):
-            pwm.ChangeDutyCycle(dc)
-            time.sleep(0.01)
-        time.sleep(1)
+#        for dc in range(0, 101, 1):
+#            pwm.ChangeDutyCycle(dc)
+#            time.sleep(0.01)
+#        time.sleep(1)
+#        for dc in range(100, -1, -1):
+#            pwm.ChangeDutyCycle(dc)
+#            time.sleep(0.01)
+#        time.sleep(1)
          
 def destroy():
     pwm.stop()
